fc0acaf17dbb5491a1c507712bb495f22fc1913e
Emma Roylance <emmers646@gmail.com>
2019-11-01 16:32:10 -0600

    JSAssignment1

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+document.write("Hello, world! ");
+var A = "This is a string";
+window.alert(A);
+var B = "Sometimes I say, \"Out with the old,\" and then someone else replies with: \"And in with the new!\" ";
+var B = B.fontcolor("yellow");
+document.write(B);
+document.write("Out with the old" + " and in with the new. ");
+var Family = "All of the people I love", Dad = "Kent", Mom = "Karalee", Boyfriend = "Ben"
+var Boyfriend = Boyfriend.fontcolor("pink");
+document.write(Boyfriend);
+document.write(4 + 5);
+
+
